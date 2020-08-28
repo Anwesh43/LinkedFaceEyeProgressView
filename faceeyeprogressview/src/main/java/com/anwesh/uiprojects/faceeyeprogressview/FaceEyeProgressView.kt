2eb9b302d11467334352089158b6110bb1d93da1
Anwesh43 <anweshthecool0@gmail.com>
2020-08-29 03:05:09 +0530

    adding FaceEyeProgress class

--- faceeyeprogressview/src/main/java/com/anwesh/uiprojects/faceeyeprogressview/FaceEyeProgressView.kt
+++ faceeyeprogressview/src/main/java/com/anwesh/uiprojects/faceeyeprogressview/FaceEyeProgressView.kt
@@ -157,6 +157,10 @@ class FaceEyeProgressView(ctx : Context) : View(ctx) {
             state.update(cb)
         }
 
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
         fun getNext(dir : Int, cb : () -> Unit) : FEPNode {
             var curr : FEPNode? = prev
             if (dir == 1) {
@@ -167,6 +171,29 @@ class FaceEyeProgressView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class FaceEyeProgress(var i : Int) {
+
+        private var curr : FEPNode = FEPNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
