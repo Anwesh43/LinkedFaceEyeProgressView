d985967598498a1d58b19e7e6980c17ec50e7d59
Anwesh43 <anweshthecool0@gmail.com>
2020-08-29 02:00:55 +0530

    adding constants and libraries

--- faceeyeprogressview/src/main/java/com/anwesh/uiprojects/faceeyeprogressview/FaceEyeProgressView.kt
+++ faceeyeprogressview/src/main/java/com/anwesh/uiprojects/faceeyeprogressview/FaceEyeProgressView.kt
@@ -3,3 +3,29 @@ package com.anwesh.uiprojects.faceeyeprogressview
 /**
  * Created by anweshmishra on 29/08/20.
  */
+
+import android.view.View
+import android.graphics.Paint
+import android.view.MotionEvent
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#009688",
+        "#3F51B5",
+        "#4CAF50",
+        "#2196F3"
+        ).map {Color.parseColor(it)}
+        .toTypedArray()
+val parts : Int = 2
+val scGap : Float = 0.02f / (parts + 1)
+val strokeFactor : Float = 90f
+val rFactor : Float = 3.8f
+val concFactor : Float = 2.8f
+val eyeFactor : Float = 8.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
