207e1cbe94abf2cd0b8da86faf9adf4d7f21d4d2
Anwesh43 <anweshthecool0@gmail.com>
2020-08-29 02:55:34 +0530

    adding FaceEyeProgressView class

--- faceeyeprogressview/src/main/java/com/anwesh/uiprojects/faceeyeprogressview/FaceEyeProgressView.kt
+++ faceeyeprogressview/src/main/java/com/anwesh/uiprojects/faceeyeprogressview/FaceEyeProgressView.kt
@@ -69,3 +69,19 @@ fun Canvas.drawFEPNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawFaceEyeProgress(scale, w, h, paint)
 }
+
+class FaceEyeProgressView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
