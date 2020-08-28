70fa3f317a4d4fce5c64c59e84c5e831019abadb
Anwesh43 <anweshthecool0@gmail.com>
2020-08-29 02:19:47 +0530

    adding Int and Float extension methods

--- faceeyeprogressview/src/main/java/com/anwesh/uiprojects/faceeyeprogressview/FaceEyeProgressView.kt
+++ faceeyeprogressview/src/main/java/com/anwesh/uiprojects/faceeyeprogressview/FaceEyeProgressView.kt
@@ -29,3 +29,8 @@ val concFactor : Float = 2.8f
 val eyeFactor : Float = 8.9f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n))
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
