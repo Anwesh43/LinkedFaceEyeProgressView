f7ff30658ffd7076ea8555e621e9eaf7cfaa4a0d
Anwesh43 <anweshthecool0@gmail.com>
2020-08-29 03:23:53 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedfaceeyeprogressview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedfaceeyeprogressview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedfaceeyeprogressview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.faceeyeprogressview.FaceEyeProgressView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        FaceEyeProgressView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
